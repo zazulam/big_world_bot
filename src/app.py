ac4de775c23171209cca6a44d8b9035a2e3d8d25
zazulam <m.zazula@gmail.com>
2022-01-15 12:16:04 -0500

    .

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from discord.ext import commands, tasks
 import os
 from itertools import cycle
 
-
+# Random comment to test workflow
 def main():
     #Initialize config & bot & intents
     intents = discord.Intents.default()
